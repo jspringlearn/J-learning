b361d24335993eed685f2faa0de5b53a231a11c5
wfef <1525304050@qq.com>
2019-03-12 21:07:19 +0800

    练习

---
+++ EG/service/src/main/java/service/ExeTest.java
@@ -0,0 +1,6 @@
+package service;
+
+public interface ExeTest {
+	String str="hello";
+
+}
---
+++ EG/service/src/test/java/service/ExeTestTest.java
@@ -0,0 +1,16 @@
+package service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExeTestTest {
+
+	@Test
+	public void test() {
+		String say="hello";
+		assertEquals(say, ExeTest.str);
+		
+	}
+
+}
