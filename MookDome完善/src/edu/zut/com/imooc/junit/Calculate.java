ff08fb73ed1f4de921e1d77854ae51fe95bdde0c
yyb <1668395108@qq.com>
2019-03-16 18:26:48 +0800

    junit完善

---
+++ MookDome完善/src/edu/zut/com/imooc/junit/Calculate.java
@@ -0,0 +1,17 @@
+package edu.zut.com.imooc.junit;
+
+public class Calculate {
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int subtract(int a,int b) {
+		return a-b;
+	}
+	public int multiply(int a,int b) {
+		return a*b;
+	}
+	public int divide(int a,int b) {
+		return a/b;
+	}
+}
