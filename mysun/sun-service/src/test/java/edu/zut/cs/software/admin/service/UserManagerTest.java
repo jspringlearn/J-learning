1e0dd3510fd960b2d1ace7115fb7417589af1fae
gsonya <1262342035@qq.com>
2019-03-17 10:42:34 +0800

    edit mysun's sun-service

--- mysun/sun-service/src/test/java/edu/zut/cs/software/admin/service/UserManagerTest.java
+++ mysun/sun-service/src/test/java/edu/zut/cs/software/admin/service/UserManagerTest.java
@@ -3,12 +3,25 @@ package edu.zut.cs.software.admin.service;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-public class UserManagerTest {
+@ContextConfiguration(locations = { "classpath:applicationContextTest-service.xml" })
+public class UserManagerTest extends AbstractJUnit4SpringContextTests{
+	
+	UserManager usermanager;
+	
+	@Autowired
+	void setusermanager(UserManager usermanager) {
+		this.usermanager=usermanager;
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSayHello() {
+		String expected="Hello world!";
+		String result=this.usermanager.sayHello("world");
+		assertEquals(expected,result);
 	}
 
 }
