bf55821634de6f3d84eb9ba299d288221b9bc9a3
gsonya <1262342035@qq.com>
2019-04-18 21:37:52 +0800

    base

---
+++ gsonya/Practice/Practice-base/src/main/java/cn/gsonya/Practice/base/service/GenericGenerator.java
@@ -0,0 +1,9 @@
+package cn.gsonya.Practice.base.service;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+@ContextConfiguration(locations= {"classpath:/applicationContextTest-resources.xml","classpath:/applicationContext-dao,xml","classpath:/applicationContext-service.xml"})
+public class GenericGenerator extends AbstractJUnit4SpringContextTests {
+
+}
