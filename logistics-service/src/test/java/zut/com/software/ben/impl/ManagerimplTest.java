e21936babe856472c678888707184232c86849fc
wfef <1525304050@qq.com>
2019-05-15 23:29:03 +0800

    签到

--- logistics-service/src/test/java/zut/com/software/ben/impl/ManagerimplTest.java
+++ logistics-service/src/test/java/zut/com/software/ben/impl/ManagerimplTest.java
@@ -8,11 +8,8 @@ public class ManagerimplTest {
 
 	@Test
 	public void test() {
-<<<<<<< HEAD
 		System.out.println("yangyb");
-=======
 		System.out.println(" %");
->>>>>>> fff3e487fba4278fd6fd41569e47e099d0415d11
 	}
 
 }
