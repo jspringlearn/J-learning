283b8db2a3836ce042d188267d83f40eea2e773d
yyb <1668395108@qq.com>
2019-05-16 22:08:05 +0800

    添加地址

---
+++ logistics-dao/src/main/java/edu/com/software/ZLoca/domain/Address.java
@@ -0,0 +1,20 @@
+package edu.com.software.ZLoca.domain;
+
+public class Address {
+	private String StartAddress;
+	private String endAddress;
+	public String getStartAddress() {
+		return StartAddress;
+	}
+	public void setStartAddress(String startAddress) {
+		StartAddress = startAddress;
+	}
+	public String getEndAddress() {
+		return endAddress;
+	}
+	public void setEndAddress(String endAddress) {
+		this.endAddress = endAddress;
+	}
+	
+
+}
