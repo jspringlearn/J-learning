8415d0b96a21c2b58e597a4e733e8159652d67d7
xiaosen <1358337137@qq.com>
2019-05-19 19:26:51 +0800

    更新更正仓库内容

---
+++ logistics_dao/src/test/java/com/stu/software/store/dao/StoreDaotest.java
@@ -0,0 +1,42 @@
+package com.stu.software.store.dao;
+
+import com.stu.software.dao.GenericTreeDaoTestCase;
+import com.stu.software.store.domain.Store;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Author xs
+ * @Date 2019/5/18 15:39
+ * @Version 1.0
+ */
+public class StoreDaotest extends GenericTreeDaoTestCase<Long, Store, StoreDao> {
+    /**
+     * Logger for this class
+     */
+    private static final Logger logger = LogManager.getLogger(StoreDaotest.class.getName());
+
+    @Resource
+    StoreDao storedao;
+
+
+    @Test
+    public void testGetRoot() {
+        int n=2;
+        for(int i = 0; i < n; i++) {
+            Store store = new Store();
+            store.setName("store_" + i);
+            this.storedao.save(store);
+        }
+
+        List<Store> roots = this.storedao.getRoot();
+        if (logger.isInfoEnabled()) {
+            logger.info("testGetRoot() - List<store> roots={}", roots); //$NON-NLS-1$
+        }
+
+    }
+}
