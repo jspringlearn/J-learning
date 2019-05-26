165eb3b5f5c43689916272ba575e23fa8ebc0123
xiaosen <1358337137@qq.com>
2019-05-26 14:04:36 +0800

    spring AOP

---
+++ doc/201708044116肖森/学习文件/Aspect2/Hello.java
@@ -0,0 +1,18 @@
+package atguigu.AspectJ;
+
+/**
+
+ */
+public class Hello
+{
+    public void sayhello()
+    {
+        System.out.println("Hello,AspectJ!");
+    }
+    public static void main(String [] args)
+    {
+        Hello hello = new Hello();
+        hello.sayhello();
+    }
+}
+
