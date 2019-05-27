b33baf456de4fb1f3163bbcc152aab20e045d6e3
2818724775 <2818724775@qq.com>
2019-05-27 09:35:14 +0800

    service

---
+++ logistics_service/src/main/java/com/stu/software/car/service/CarManager.java
@@ -0,0 +1,8 @@
+package com.stu.software.car.service;
+
+import com.stu.software.car.domain.Car;
+import com.stu.software.service.GenericManager;
+
+public interface CarManager extends GenericManager<Car, Long> {
+
+}
---
+++ logistics_service/src/main/java/com/stu/software/car/service/GarageManager.java
@@ -0,0 +1,8 @@
+package com.stu.software.car.service;
+
+import com.stu.software.car.domain.Garage;
+import com.stu.software.service.GenericTreeManager;
+
+public interface GarageManager extends GenericTreeManager<Garage, Long> {
+
+}
