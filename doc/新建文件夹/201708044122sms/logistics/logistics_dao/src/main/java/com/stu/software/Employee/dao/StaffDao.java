d8c0b1d3457c802d4bc0e4786b7d2815562772ed
ZXF1999 <2025840567@qq.com>
2019-05-30 18:29:18 +0800

    清除

--- doc/新建文件夹/201708044122sms/logistics/logistics_dao/src/main/java/com/stu/software/Employee/dao/StaffDao.java
+++
@@ -1,7 +0,0 @@
-package com.stu.software.Employee.dao;
-
-import com.stu.software.Employee.domain.Staff;
-import com.stu.software.dao.GenericDao;
-
-public interface StaffDao extends GenericDao<Staff,Long> {
-}
