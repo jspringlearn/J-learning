f605a0dff3893af2af0f44442c8d4be0edf94683
loumengyue <2751305257@qq.com>
2019-05-31 00:55:20 +0800

    TypesDaoTest.java OrderDaoTest.java

--- logistics_dao/src/test/java/com/stu/software/order/dao/OrderDaoTest.java
+++ logistics_dao/src/test/java/com/stu/software/order/dao/OrderDaoTest.java
@@ -1,46 +1,41 @@
 package com.stu.software.order.dao;
 
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.stu.software.dao.GenericDaoTestCase;
-import com.stu.software.dao.GenericTreeDaoTestCase;
 import com.stu.software.order.dao.OrderDao;
 import com.stu.software.order.domain.Order;
 
+
+
 public class OrderDaoTest extends GenericDaoTestCase<Long, Order, OrderDao> {
 
-	
-	OrderDao orderDao;
-	    @Autowired
-	    public void setOrderDao(OrderDao orderDao){
-	        this.orderDao=orderDao;
-	        this.dao=this.orderDao;
-	    }
-	    @Test
-	    public void testGetRoot(){
-	        int root_size=10;
-	        for(int i=0;i<root_size;i++){
-	            Order order=new Order();
-	            order.setOrderID("order_"+i);
-	            for(int j=0;j<10;j++) {
-	                Order g=new Order();
-	                g.setOrderID("order_"+i+"_"+j);
-//	                g.setParent(order);
-	     
-	            }
-	            this.orderDao.save(order);
-	        }
-//	        List<Order> roots=this.orderDao.getRoot();
-//	        assertEquals(root_size,roots.size());
 
-//	        if (logger.isInfoEnabled()) {
-//	            logger.info("testGetRoot() - List<Boss> roots={}", roots); //$NON-NLS-1$
-	        
-	    }
+	  private static final Logger logger = LogManager.getLogger(OrderDaoTest.class.getName());
+
+	    @Autowired
+	    OrderDao orderDao;
+
+	    @Test
+	        public void testGetRoot() {
+	           int n=2;
+	            for(int i = 1; i <= n; i++) {
+	                Order order = new Order();
+	                order.setOrderName("order_" + i);
+	                order.setOrderName("order_" + i);
+	                this.orderDao.save(order);
+	            }
+
+	        List<Order> all = this.orderDao.findAll();
+	        if (logger.isInfoEnabled()) {
+	            logger.info("test() - List<order> all size ={}", all.size()); //$NON-NLS-1$
+	        }
+
+	        }
 }
