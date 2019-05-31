a9a7278c68d960fdb0830d7f01639451a814dc5f
zhangben <506755508@qq.com>
2019-05-31 23:59:45 +0800

    修改dao层

--- logistics_dao/src/test/java/com/stu/software/place/dao/SiteGroupDaoTest.java
+++ logistics_dao/src/test/java/com/stu/software/place/dao/SiteGroupDaoTest.java
@@ -1,5 +1,7 @@
 package com.stu.software.place.dao;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -12,28 +14,58 @@ import com.stu.software.dao.GenericTreeDaoTestCase;
 import com.stu.software.place.domain.SiteGroup;
 
 
+
+
 public class SiteGroupDaoTest  extends GenericTreeDaoTestCase<Long, SiteGroup, SiteGroupDao>{
 
 	  private static final Logger logger = LogManager.getLogger(SiteGroupDaoTest.class.getName());
 
-	    @Autowired
+	    
 	    SiteGroupDao siteGroupDao;
 
+
+	    @Autowired
+		public void setSiteGroupDao(SiteGroupDao siteGroupDao) {
+			this.siteGroupDao = siteGroupDao;
+			this.dao=this.siteGroupDao;
+		}
+
+		//	    @Test
+//	        public void testGetRoot() {
+//	           int n=2;
+//	            for(int i = 1; i <= n; i++) {
+//	            	SiteGroup siteGroup = new SiteGroup();
+//	            	siteGroup.setGroupName("siteGroup_" + i);
+//	            	siteGroup.setGroupName("siteGroup_" + i);
+//	                this.siteGroupDao.save(siteGroup);
+//	            }
+//
+//	        List<SiteGroup> all = this.siteGroupDao.findAll();
+//	        if (logger.isInfoEnabled()) {
+//	            logger.info("test() - List<siteGroup> all size ={}", all.size()); //$NON-NLS-1$
+//	        }
+//
+//	        }
 	    @Test
-	        public void testGetRoot() {
-	           int n=2;
-	            for(int i = 1; i <= n; i++) {
-	            	SiteGroup siteGroup = new SiteGroup();
-	            	siteGroup.setGroupName("siteGroup_" + i);
-	            	siteGroup.setGroupName("siteGroup_" + i);
-	                this.siteGroupDao.save(siteGroup);
-	            }
+	    public void testGetRoot() {
+			int root_size = 10;
+			for (int i = 0; i < root_size; i++) {
+				SiteGroup group = new SiteGroup();
+				group.setGroupName("group_" + i);
+				for (int j = 0; j < 10; j++) {
+					SiteGroup g = new SiteGroup();
+					g.setGroupName("group_" + i + "_" + j);
+					g.setParent(group);
+				}
+				this.siteGroupDao.save(group);
+			}
 
-	        List<SiteGroup> all = this.siteGroupDao.findAll();
-	        if (logger.isInfoEnabled()) {
-	            logger.info("test() - List<siteGroup> all size ={}", all.size()); //$NON-NLS-1$
-	        }
+			List<SiteGroup> roots = this.siteGroupDao.getRoot();
+			assertEquals(root_size, roots.size());
 
-	        }
+			if (logger.isInfoEnabled()) {
+				logger.info("testGetRoot() - List<Group> roots=" + roots); //$NON-NLS-1$
+			}
+		}
 
 }
