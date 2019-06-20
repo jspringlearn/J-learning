e91c91a88993533e45130ca2e666316a05a8dd34
ZXF1999 <2025840567@qq.com>
2019-06-20 19:33:34 +0800

    update

--- logistics_service/src/main/java/com/stu/software/feedback/service/CommentManager.java
+++ logistics_service/src/main/java/com/stu/software/feedback/service/CommentManager.java
@@ -5,12 +5,17 @@ import com.stu.software.service.GenericManager;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 
 @Component
 @Transactional
 public interface CommentManager extends GenericManager<Comment, Long> {
 
-    Comment findbyCommentID(String commentID);
+    List<Comment> findbyCommentID(String commentID);
+    void savecomment(String commentID,String commentContent,String commentTime);
+    void deletecomment(String commentID);
+    void upd(Comment comment);
+    Comment findone(String commentID);
 
 }
