a82b2dc78edc1cbea5d5407aea4e9039058193a2
ZXF1999 <2025840567@qq.com>
2019-06-26 10:53:07 +0800

    修改

--- logistics_service/src/main/java/com/stu/software/feedback/service/CommentManager.java
+++ logistics_service/src/main/java/com/stu/software/feedback/service/CommentManager.java
@@ -11,11 +11,25 @@ import java.util.List;
 @Component
 @Transactional
 public interface CommentManager extends GenericManager<Comment, Long> {
-
+    /*
+     * 罗列出所有评论的信息
+     *
+     * */
     List<Comment> findAll();
+    /*
+     * 保存评论信息
+     *
+     * */
     Comment save(Comment comment);
+    /*
+     * 根据id删除评论
+     *
+     * */
     void delete(Long id);
-
+    /*
+     * 根据id查找某条评论
+     *
+     * */
     Comment findById(Long id);
 
 }
